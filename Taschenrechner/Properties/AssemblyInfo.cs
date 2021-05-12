ab41fadc63f52601d8c5d51718def9f31086b0ec
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-12 22:59:51 +0200

    adjustment Assembly Properties

--- Taschenrechner/Properties/AssemblyInfo.cs
+++ Taschenrechner/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Taschenrechner_Iteration_1")]
+[assembly: AssemblyTitle("Taschenrechner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Taschenrechner_Iteration_1")]
+[assembly: AssemblyProduct("Taschenrechner")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
