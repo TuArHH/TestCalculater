0904cc2720ea275d3b79868d5f4390cb5d9b855d
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-12 23:19:48 +0200

    Adding Release file

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Taschenrechner
 {
@@ -34,12 +30,7 @@ namespace Taschenrechner
 
             }
            
-            
-
-            
-            
-            
-
+          
          
             Console.ReadKey();
 
