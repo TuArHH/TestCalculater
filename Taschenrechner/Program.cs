b833c7c0172a4ae8df854d65f727da526073ee80
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-13 00:05:23 +0200

    Adding a Metod, that compute two values of double datatypes

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -10,40 +10,33 @@ namespace Taschenrechner
             /**
              *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe berechnen zu lassen
              **/
-             
-            try
-            {
-                Console.Write("Bitte geben den ersten Summanden ein:");
-                double doubnum1 = double.Parse(Console.ReadLine());
-                Console.Write("Bitte geben den zweiten Summanden ein:");
-                double doubnum2 = double.Parse(Console.ReadLine());
 
-                
-                double resultdouble = AddierenDouble(doubnum1, doubnum2);
-                Console.WriteLine("Das Ergebnis lautet \t:{0} ", resultdouble);
-               
-                
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Bitte geben Sie eine Zahl ein ");
 
-            }
-           
-          
-         
+
+
+
+
+
+
+            Console.Write("Bitte geben den ersten Summanden ein:");
+            double doubnum1 = double.Parse(Console.ReadLine());
+            Console.Write("Bitte geben den zweiten Summanden ein:");
+            double doubnum2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Das Ergebnis ist \t:{0} ",Addieren(doubnum1, doubnum2)+Environment.NewLine+"Bitte drücken sie die EnterTaste um das Programm zu beenden");
+
             Console.ReadKey();
 
         }
 
-        public static int Addieren(int num1,int num2)
-        {   int result; 
-            return result = num1 + num2;
-        }
+      
 
-         public static double AddierenDouble(double num1,double num2)
-        {   double result; 
-            return result = num1 + num2;
+         public static double Addieren(double num1,double num2)
+        {
+            
+            double result;
+            return result= num1 + num2;
+            
+
         }
     }
 }
