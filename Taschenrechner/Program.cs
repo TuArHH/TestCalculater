97a99ef7fd2549879bafc435cfad877efa4cb0b0
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-16 15:34:26 +0200

    edit new variable names and edit orther convert Class (Parse --> ConvertToint32)

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -5,38 +5,53 @@ namespace Taschenrechner
 
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            /**
-             *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe berechnen zu lassen
-             **/
+                /**
+                 *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe berechnen zu lassen
+                 **/
+             
+                Console.Write("Bitte geben den ersten Summanden ein:");
+                string summand1 = Console.ReadLine();
+
+                Console.Write("Bitte geben den zweiten Summanden ein:");
+                string summand2 = Console.ReadLine();
+
+                // Wandeln in einen Ganzzahle
+                int num1 = Convert.ToInt32(summand1);
+                int num2 = Convert.ToInt32(summand2);
+
+                //Ausgabe
+                Console.WriteLine("Das Ergebnis ist \t:{0} ", Addieren(num1, num2) + Environment.NewLine + 
+                "Bitte drücken sie die EnterTaste um das Programm zu beenden");
+
+                Console.ReadKey();
+
+           
+
+    }
+    
 
 
-
-
-
-
-
-
-            Console.Write("Bitte geben den ersten Summanden ein:");
-            double doubnum1 = double.Parse(Console.ReadLine());
-            Console.Write("Bitte geben den zweiten Summanden ein:");
-            double doubnum2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Das Ergebnis ist \t:{0} ",Addieren(doubnum1, doubnum2)+Environment.NewLine+"Bitte drücken sie die EnterTaste um das Programm zu beenden");
-
-            Console.ReadKey();
-
-        }
-
-      
-
-         public static double Addieren(double num1,double num2)
+        public static double Addieren(double num1,double num2)
         {
-            
+                
+                double result;
+                return result = num1 + num2;
+           
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+
             double result;
-            return result= num1 + num2;
-            
+            return result = num1 - num2;
 
         }
+
+
     }
 }
