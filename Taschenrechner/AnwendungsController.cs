950502ff5327894c5f45aec1234d8d1a9aaa263d
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-17 01:12:29 +0200

    add a new Object 'AnwenungsController'

---
+++ Taschenrechner/AnwendungsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class AnwendungsController
+    {
+        private RechnerModel model;
+
+        public AnwendungsController(RechnerModel model)
+        {
+            this.model = model;
+        }
+
+        public void Ausfuehren()
+        {
+            model.Berechne();
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -11,14 +11,15 @@ namespace Taschenrechner
         static void Main(string[] args)
         {
             /**
-             *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe berechnen zu lassen
+             *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe,Diffzerenz, Division und Mulitpliaktion berechnen zu lassen
              **/
 
-            //Berechne ausführen
+            //Objekte declariert und initalisiert
             RechnerModel model = new RechnerModel();
-            model.Berechne();
-            
-            //Ausgabe 
+            AnwendungsController controller = new AnwendungsController(model);
+
+            //Ausgabe
+            controller.Ausfuehren();
             Console.ReadKey();
 
            
