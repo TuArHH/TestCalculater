52698bc3e4cb73ed1a0a7316e37e8f3551e07465
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-17 00:51:31 +0200

    Create a Objetct and  and outsourced the compute step

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -14,8 +14,11 @@ namespace Taschenrechner
              *  Als Benutzer möchte ich zwei Zahlen eingeben [Ganzahlen & Gleitkommazahlen], um deren Summe berechnen zu lassen
              **/
 
+            //Berechne ausführen
+            RechnerModel model = new RechnerModel();
+            model.Berechne();
+            
             //Ausgabe 
-            Output();
             Console.ReadKey();
 
            
@@ -24,84 +27,8 @@ namespace Taschenrechner
     
 
 
-        public static double Addieren(double num1,double num2)
-        {
-                
-                return  num1 + num2;
-        }
 
-        public static double Subtract(double num1, double num2)
-        {
-
-            
-            return  num1 - num2;
-
-        }
-
-        public static double Divided(double num1,double num2)
-        {
-            
-            return  num1 / num2;
-        }
         
-        public static double Muliplication(double num1,double num2)
-        {
-            
-            return  num1 * num2;
-        }
-
-        public static string Operation(string op)
-        {
-            Console.Write(op);
-            string summand = Console.ReadLine();
-            return summand;
-        }
-
-        public static void Output()
-        {
-            Console.Write("Bitte gebe die erste Zahl ein:");
-            string summand1 = Console.ReadLine();
-
-            Console.Write("Bitte gebe die zweite Zahl ein:");
-            string summand2 = Console.ReadLine();
-
-            string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
-
-            //Wandeln in einen Gleitkommazahl
-            double dnum1 = Convert.ToDouble(summand1);
-            double dnum2 = Convert.ToDouble(summand2);
-            double result = 0;
-            // Berechnung verschiedener Operationen
-            if (operation == "+")
-            {
-                result = Addieren(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", result + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else if (operation == "-")
-            {
-                result = Subtract(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", result + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else if (operation == "/")
-            {
-                result = Divided(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", result + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else if (operation == "*")
-            {
-                result = Muliplication(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", result + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else
-            {
-                Console.WriteLine("Diese Operation existiert in diesem Kontext nicht");
-            }
-
-        }
 
     }
 }
---
+++ Taschenrechner/RechnerModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class RechnerModel
+    {
+        
+
+        public double Resultat { get; private set; }
+
+        public void Rechnermodel()
+        {
+            Resultat = 0;
+        }
+
+
+
+        
+
+        public static double Addieren(double num1, double num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+
+
+            return num1 - num2;
+
+        }
+
+        public static double Divided(double num1, double num2)
+        {
+
+            return num1 / num2;
+        }
+
+        public static double Muliplication(double num1, double num2)
+        {
+
+            return num1 * num2;
+        }
+
+        public static string Operation(string op)
+        {
+            Console.Write(op);
+            string summand = Console.ReadLine();
+            return summand;
+        }
+
+        public void Berechne()
+        {
+
+            Console.Write("Bitte gebe die erste Zahl ein:");
+            string summand1 = Console.ReadLine();
+            Console.Write("Bitte gebe die zweite Zahl ein:");
+            string summand2 = Console.ReadLine();
+            string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
+
+            // Wanden in Zahlen
+            double dnum1 = Convert.ToDouble(summand1);
+            double dnum2 = Convert.ToDouble(summand2);
+
+            // Berechnung verschiedener Operationen
+            if (operation == "+")
+            {
+                Resultat = Addieren(dnum1, dnum2);
+                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                "Bitte drücke die EnterTaste um das Programm zu beenden");
+            }
+            else if (operation == "-")
+            {
+                Resultat = Subtract(dnum1, dnum2);
+                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                "Bitte drücke die EnterTaste um das Programm zu beenden");
+            }
+            else if (operation == "/")
+            {
+                Resultat = Divided(dnum1, dnum2);
+                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                "Bitte drücke die EnterTaste um das Programm zu beenden");
+            }
+            else if (operation == "*")
+            {
+                Resultat = Muliplication(dnum1, dnum2);
+                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                "Bitte drücke die EnterTaste um das Programm zu beenden");
+            }
+            else
+            {
+                Console.WriteLine("Diese Operation existiert in diesem Kontext nicht");
+            }
+        }
+
+        
+    }
+}
