5c393ea198961ccf309d91d4adc7db62998b12de
Tugrul Arslan <tugrul.arslan@gmx.de>
2021-05-18 16:34:26 +0200

    implement try catch block --> If the user enter a string , the rogramm would be ending

--- Taschenrechner/RechnerModel.cs
+++ Taschenrechner/RechnerModel.cs
@@ -54,49 +54,82 @@ namespace Taschenrechner
             return summand;
         }
 
+        //public void GetNumber()
+        //{
+        //    double result;
+        //    Console.Write("Bitte gebe die erste Zahl ein:");
+        //    string summand1 = Console.ReadLine();
+        //    string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
+        //    Console.Write("Bitte gebe die zweite Zahl ein:");
+        //    string summand2 = Console.ReadLine();
+
+        //    // Wanden in Zahlen
+        //    double dnum1 = double.Parse(summand1);
+        //    double dnum2 = double.Parse(summand2);
+        //}
+
         public void Berechne()
         {
-
-            Console.Write("Bitte gebe die erste Zahl ein:");
-            string summand1 = Console.ReadLine();
-            string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
-            Console.Write("Bitte gebe die zweite Zahl ein:");
-            string summand2 = Console.ReadLine();
-           
+            //double result;
+            //Console.Write("Bitte gebe die erste Zahl ein:");
+            //double summand1 = double.Parse(Console.ReadLine());
+            //string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
+            //Console.Write("Bitte gebe die zweite Zahl ein:");
+            //double  summand2 = double.Parse(Console.ReadLine());
 
             // Wanden in Zahlen
-            double dnum1 = Convert.ToDouble(summand1);
-            double dnum2 = Convert.ToDouble(summand2);
+            //double dnum1 = double.Parse(summand1);
+            //double dnum2 = double.Parse(summand2);
 
-            // Berechnung verschiedener Operationen
-            if (operation == "+")
+            
+
+
+            try
             {
-                Resultat = Addieren(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
+                
+                Console.Write("Bitte gebe die erste Zahl ein:");
+                double summand1 = double.Parse(Console.ReadLine());
+                string operation = Operation("Bitte gib die auszuführende Operation ein (+ , - , / oder *) : ");
+                Console.Write("Bitte gebe die zweite Zahl ein:");
+                double summand2 = double.Parse(Console.ReadLine());
+
+
+                // Berechnung verschiedener Operationen
+                if (operation == "+")
+                {
+                    Resultat = Addieren(summand1, summand2);
+                    Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                    "Bitte drücke die EnterTaste um das Programm zu beenden");
+                }
+                else if (operation == "-")
+                {
+                    Resultat = Subtract(summand1, summand2);
+                    Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                    "Bitte drücke die EnterTaste um das Programm zu beenden");
+                }
+                else if (operation == "/" || operation == ":")
+                {
+                    Resultat = Divided(summand1, summand2);
+                    Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                    "Bitte drücke die EnterTaste um das Programm zu beenden");
+                }
+                else if (operation == "*")
+                {
+                    Resultat = Muliplication(summand1, summand2);
+                    Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
+                    "Bitte drücke die EnterTaste um das Programm zu beenden");
+                }
+                else
+                {
+                    Console.WriteLine("Diese Operation existiert in diesem Kontext nicht");
+                }
             }
-            else if (operation == "-")
+            catch (Exception)
             {
-                Resultat = Subtract(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
+
+                Console.WriteLine("Bitte gebe nur (primitve) Zahlen ein");
             }
-            else if (operation == "/" || operation == ":" )
-            {
-                Resultat = Divided(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else if (operation == "*")
-            {
-                Resultat = Muliplication(dnum1, dnum2);
-                Console.WriteLine("Das Ergebnis ist \t:{0} ", Resultat + Environment.NewLine +
-                "Bitte drücke die EnterTaste um das Programm zu beenden");
-            }
-            else
-            {
-                Console.WriteLine("Diese Operation existiert in diesem Kontext nicht");
-            }
+
         }
 
         
